96f2e7f746443dac1465e5d5ef1f5cbed49c8c64
gheeeeeeee <gheoputra55@gmail.com>
2024-03-29 14:54:00 +0700

    langkah ke sembilan dalam pembuatan q short, menukarkan i dan j apabila i lebih kecil dari pada j

--- QuickSort_0058/QuickSort_0058.cpp
+++ QuickSort_0058/QuickSort_0058.cpp
@@ -72,6 +72,12 @@ void q_short(int low, int high)
 		j--;//step 8
 		cmp_count++;
 	}
+	cmp_count++;
+	if (i < j) // step 9
+	{
+		//swap the element at index i dengan index j
+		swap(i, j);
+	}
 
 }
 
