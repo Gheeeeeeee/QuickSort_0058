3887b86c25b1b258acc0721e5825c882faacddc3
gheeeeeeee <gheoputra55@gmail.com>
2024-03-29 14:50:28 +0700

    langkah ke tujuh dalam membuat q short

--- QuickSort_0058/QuickSort_0058.cpp
+++ QuickSort_0058/QuickSort_0058.cpp
@@ -63,6 +63,13 @@ void q_short(int low, int high)
 	}	while ((arr[i] <= pivot) && (i <= high)) /// step 5
 	{
 		i++;//step 6
+		cmp_count++;
+	}
+	cmp_count++;
+	//mencari element yang lebih kecil atau sama dengan pivot
+	while ((arr[j] > pivot) && (j >= low)) //step7
+	{
+
 	}
 
 }
